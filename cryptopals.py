e893bc220f7879aa129dd90853d5b0fbab90623a
ABP <akaash-b-p@hotmail.com>
2020-09-13 20:07:59 +0200

    c13 improved, 14 skipped, 15 finished

--- cryptopals.py
+++ cryptopals.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import numpy as np
 import base64 as b64
 from collections import Counter
@@ -221,6 +222,64 @@ def random_AES_key():
     return secrets.token_bytes(16)
 
 
+def profile_for(email):
+    profile = parse_profile(email)
+    data = pad(16, profile, multiples_allowed=True)
+    encrypted_user_data = ECB("encrypt", b"PASSWORDPASSWORD", data)
+    return encrypted_user_data
+
+
+def profile_decrypt(data):
+    decrypted_user_data = ECB("decrypt", b"PASSWORDPASSWORD", data)
+    return decrypted_user_data
+
+
+def unpack_profile(data):
+    return {
+        decode(key): decode(value)
+        for key, value in (line.split(b"=") for line in data.split(b"&"))
+    }
+
+
+def parse_profile(email):
+    if 0 < email.count("=") + email.count("&"):
+        raise Exception("Invalid character encountered")
+    return encode(f"email={email}&uid=10&role=user")
+
+
+def buffer_creator(position, append, keylength):
+    #create buffers of length 15
+    return (b"0" * (keylength - position - 1) + append)
+
+
+def encryption_oracle(data):
+    data = secrets.token_bytes(
+        secrets.randbelow(5)) + data + secrets.token_bytes(
+            secrets.randbelow(5))
+    data_p = pad(16, data, multiples_allowed=True)
+    if secrets.choice([True, False]):
+        mode = "ECB"
+        result = ECB("encrypt", random_AES_key(), data_p)
+    else:
+        mode = "CBC"
+        result = CBC("encrypt", secrets.token_bytes(16), random_AES_key(),
+                     data_p)
+    return result, mode
+
+
+def ECB_oracle(data):
+    ECB_oracle.key = getattr(ECB_oracle, 'key', secrets.token_bytes(16))
+    data += b64.b64decode(import_data("data_S2C12.txt"))
+    data = pad(16, data, multiples_allowed=True)
+    return ECB("encrypt", ECB_oracle.key, data)
+
+
+def ECB_mode_check(data):
+    array = np.frombuffer(data, dtype="uint8").reshape(-1, 16)
+    duplicate_blocks = len(array) - len(np.unique(array, axis=0))
+    return True if 0 < duplicate_blocks else False
+
+
 class set_1():
     def challenge_1(self):
         print("\n-- Challenge 1 - Convert hex to base 64 --")
@@ -375,14 +434,11 @@ class set_1():
         print(f"-- Method 2 --")
 
         #find proportion of unique lines
-        for line_index, line in enumerate(hex_ciphertext.splitlines()):
-            data = bytes.fromhex(line)
-            array = np.frombuffer(data, dtype="uint8").reshape(10, 16)
-            if ECB_mode_check(data):
-                break
+        for line_index2, line in enumerate(hex_ciphertext.splitlines()):
+            if ECB_mode_check(bytes.fromhex(line)): break
 
         print(f"Find line with least duplicate chunks")
-        print(f"Corresponding line             : {line_index}")
+        print(f"Corresponding line              : {line_index2}")
 
 
 class set_2():
@@ -467,57 +523,102 @@ class set_2():
 
     def challenge_13(self):
         print(f"\n-- Challenge 13 - ECB cut-and-paste --")
-        text = """{"foo": "bar", "bad": "qux", "zap": "zazzle"}"""
-        print(
-            f"Example k=v parse of string : {text} to dictionary : {key_value_creator(text)}"
-        )
+        print(f"-- Part 1 --")
 
-        user1 = "user@attack.com"
-        encrypted_user_data = profile_for(user1)
-        decrypted_user_data = profile_decrypt(encrypted_user_data)
-        data_len = len(encrypted_user_data)
+        text = """foo=bar&baz=qux&zap=zazzle"""
+        email = "hello@world.com"
+        parsed = parse_profile(email)
 
-        print(f"user : {user1}")
-        print(f"Encrypted user data : {encrypted_user_data}")
-        print(f"Decrypted user data : {decrypted_user_data}")
-        print()
+        print(f"Example string   : {text}")
+        print(f"Unpacked profile : {unpack_profile(encode(text))}")
+        print(f"Example Email    : {email}")
+        print(f"Parsed profile   : {parsed}")
+        print(f"Unpacked profile : {unpack_profile(parsed)}")
+        print(f"-- Part 2 --")
 
-        user2 = user1
-        # create an email with a length that adds a full pad block to output
-        while len(encrypted_user_data) == data_len:
-            user2 = "u" + user2
-            encrypted_user_data = profile_for(user2)
-        print(user2)
-        print(profile_decrypt(profile_for(user2)))
-        print()
-        guessed_ending_format = "'user'}"
-        bytes_to_remove = len(guessed_ending_format)
-        #push bytes to remove into new block
-        user3 = 'u' * (bytes_to_remove) + user2
-        print(user3)
+        base_email = "user@hack.com"
+        base_encryption = profile_for(base_email)
+        base_encryption_len = len(base_encryption)
+        base_decryption = profile_decrypt(base_encryption)
 
-        # cut useful bytes
-        cut1 = profile_for(user3)[0:64]
-        print(cut1)
+        print(f"Base email        : {base_email}")
+        print(f"Encrypted profile : {base_encryption}")
+        print(f"Encrypted length  : {base_encryption_len}")
+        print(f"Decrypted data    : {base_decryption}")
 
-        #craft new ending for data
-        new_end = """"admin"}"""
-        replacement = decode(pad(16, encode(new_end), multiples_allowed=True))
-        print(replacement)
-        #find replaceable bytes by sending repeating letters
-        print(profile_for(user1))
-        print(profile_for("qwertuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" + user1))
-        # so 5 is the alignment
-        # add custom input after 5 bytes
-        replacement = encode(new_end) + bytes([8]) * 8
+        #create an email that creates a whole new block in output
+        end_align_email = base_email
+        end_align_encryption = base_encryption
+        while len(end_align_encryption) == base_encryption_len:
+            end_align_email = "a" + end_align_email
+            end_align_encryption = profile_for(end_align_email)
+        end_align_encryption_len = len(end_align_encryption)
 
-        profile_new = "qwertuuuuuuuuuuuuuuuu" + decode(replacement)
-        print(encode(profile_new))
-        cut2 = profile_for(profile_new)[32:48]
-        print(profile_decrypt(cut2))
+        print(f"End aligning email : {end_align_email}")
+        print(f"Encrypted profile  : {end_align_encryption}")
+        print(f"Encrypted length   : {end_align_encryption_len}")
 
-        cypher = cut1 + cut2
-        print(profile_decrypt(cypher))
+        # add bytes to push unwanted data from encryption into end block and crop useful blocks
+        bytes_to_remove = len(b"user")
+        crop_email = ("b" * bytes_to_remove) + end_align_email
+        crop = profile_for(crop_email)[0:48]
+        decryption = profile_decrypt(crop)
+
+        print(f"bytes to push into new block : {bytes_to_remove}")
+        print(f"Crop aligning email          : {crop_email}")
+        print(f"Encrypted profile crop       : {crop}")
+        print(f"Crop length                  : {len(crop)}")
+        print(f"Decrypted crop               : {decryption}")
+
+        # create an email that shows its position in the encryption
+        position_email = base_email
+        #look for two identical blocks in encryption
+        while not ECB_mode_check(profile_for(position_email)):
+            position_email = "c" + position_email
+
+        print(f"Position finding email : {position_email}")
+
+        #find position at which duplicated block starts changing
+        position = 0
+        while ECB_mode_check(profile_for(position_email)):
+            position_email_list = list(position_email)
+            position_email_list[position] = "d"
+            position_email = "".join(position_email_list)
+            position += 1
+        position -= 1
+
+        print(f"Position finding email  : {position_email}")
+        print(f"Position of block start : {position}")
+
+        bytes_to_add = position - len(base_email)
+        if bytes_to_add < 0: bytes_to_add += 16
+        block_end_email = ("e" * bytes_to_add) + base_email
+
+        print(f"Bytes to add to email : {bytes_to_add}")
+        print(f"Email ending block    : {block_end_email}")
+
+        # craft new ending for encrypted data
+        new_end = decode(pad(16, b"admin", multiples_allowed=True))
+        new_end_encryption_email = block_end_email + new_end
+        cut = profile_for(new_end_encryption_email)[32:48]
+        decrypted_cut = profile_decrypt(cut)
+
+        print(f"new end encrypting email : {new_end_encryption_email}")
+        print(f"new ending encryption    : {cut}")
+        print(f"new ending decrypted     : {decrypted_cut}")
+
+        attacker_encrypted_profile = crop + cut
+        attacker_decrypted_profile = profile_decrypt(
+            attacker_encrypted_profile)
+        attacker_profile = unpack_profile(depad(attacker_decrypted_profile))
+
+        print(f"Attacker encrypted profile : {attacker_encrypted_profile}")
+        print(f"Attacker decrypted profile : {attacker_decrypted_profile}")
+        print(f"Attacker  profile          : {attacker_profile}")
+
+    def challenge_14(self):
+        print(f"\n-- Challenge 14 - Byte-at-a-time ECB decryption (Harder) --")
+        #find bytes needed to align end of blocks
 
     def challenge_15(self):
         print(f"\n-- Challenge 15 - PKCS#7 padding validation --")
@@ -533,82 +634,30 @@ class set_2():
                 print(f"Depad of {i} : {e}")
 
 
-def profile_for(email):
-    profile = encode(user_profile_encoder(email))
-    data = pad(16, profile, multiples_allowed=True)
-    encrypted_user_data = ECB("encrypt", b"PASSWORDPASSWORD", data)
-    return encrypted_user_data
-
-
-def profile_decrypt(data):
-    decrypted_user_data = ECB("decrypt", b"PASSWORDPASSWORD", data)
-    return decrypted_user_data
-    #return key_value_creator(depad(decrypted_user_data))
-
-
-def key_value_creator(data):
-    return json.loads(data)
-
-
-def user_profile_encoder(email):
-    if 0 < email.count(":") + email.count(","):
-        raise Exception("Invalid character encountered")
-    return f"email={email}&uid={10}&role={"user"}"
-
-
-def buffer_creator(position, append, keylength):
-    #create buffers of length 15
-    return (b"0" * (keylength - position - 1) + append)
-
-
-def encryption_oracle(data):
-    data = secrets.token_bytes(
-        secrets.randbelow(5)) + data + secrets.token_bytes(
-            secrets.randbelow(5))
-    data_p = pad(16, data, multiples_allowed=True)
-    if secrets.choice([True, False]):
-        mode = "ECB"
-        result = ECB("encrypt", random_AES_key(), data_p)
-    else:
-        mode = "CBC"
-        result = CBC("encrypt", secrets.token_bytes(16), random_AES_key(),
-                     data_p)
-    return result, mode
-
-
-def ECB_oracle(data):
-    ECB_oracle.key = getattr(ECB_oracle, 'key', secrets.token_bytes(16))
-    data += b64.b64decode(import_data("data_S2C12.txt"))
-    data = pad(16, data, multiples_allowed=True)
-    return ECB("encrypt", ECB_oracle.key, data)
-
-
-def ECB_mode_check(data):
-    array = np.frombuffer(data, dtype="uint8").reshape(-1, 16)
-    duplicate_blocks = len(array) - len(np.unique(array, axis=0))
-    return True if 0 < duplicate_blocks else False
-
-
 def main():
     print("\n\n-- The Cryptopals Crypto Challenges in Python by Akaash BP --")
     # https://cryptopals.com
+    startTime = time.time()
 
-    #set_1().challenge_1()
-    #set_1().challenge_2()
-    #set_1().challenge_3()
-    #set_1().challenge_4()
-    #set_1().challenge_5()
-    #set_1().challenge_6()
-    #set_1().challenge_7()
-    #set_1().challenge_8()
+    set_1().challenge_1()
+    set_1().challenge_2()
+    set_1().challenge_3()
+    set_1().challenge_4()
+    set_1().challenge_5()
+    set_1().challenge_6()
+    set_1().challenge_7()
+    set_1().challenge_8()
 
-    #set_2().challenge_9()
-    #set_2().challenge_10()
-    #set_2().challenge_11()
-    #set_2().challenge_12()
+    set_2().challenge_9()
+    set_2().challenge_10()
+    set_2().challenge_11()
+    set_2().challenge_12()
     set_2().challenge_13()
-    #set_2().challenge_14()
-    #set_2().challenge_15()
+    set_2().challenge_14()
+    set_2().challenge_15()
+
+    executionTime = (time.time() - startTime)
+    print('\nExecution time in seconds: ' + str(executionTime))
 
 
 if __name__ == "__main__":
