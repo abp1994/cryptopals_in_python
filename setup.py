274a68a9b15f7d066f285a4ab54afefc5188fe31
ABP <71227493+abp1994@users.noreply.github.com>
2020-10-06 18:04:43 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='TowelStuff',
+    version='0.1dev',
+    packages=find_packages(),
+    license='CC0',
+    long_description=open('README.txt').read(),
+)
