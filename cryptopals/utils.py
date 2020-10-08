75c8d383e7f4b2ca501399e7664c3f013cca1f0f
ABP <71227493+abp1994@users.noreply.github.com>
2020-10-08 16:16:47 +0200

    creation of new utility function file

---
+++ cryptopals/utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from pathlib import Path
+
+
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+
+def import_data(file_name):
+    file_path = str(Path(__file__).parent.resolve() / "data/" / file_name)
+    with open(file_path) as f:
+        return f.read()
+
+
+def encode(text):
+    return text.encode("utf-8")
+
+
+def decode(byte_array):
+    return byte_array.decode("utf-8")
