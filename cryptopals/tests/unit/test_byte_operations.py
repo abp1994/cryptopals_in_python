f6de7a8e54993bb7553ff5b89e8f1a3f7f4f365b
adollar <71227493+abp1994@users.noreply.github.com>
2025-07-15 23:32:22 +0100

    added _ to unused outputs

--- cryptopals/tests/unit/test_byte_operations.py
+++ cryptopals/tests/unit/test_byte_operations.py
@@ -51,4 +51,4 @@ class Testbyte_operations(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    _ = unittest.main()
--- cryptopals/tests/unit/test_utils.py
+++ cryptopals/tests/unit/test_utils.py
@@ -13,4 +13,4 @@ class TestUtils(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    _ = unittest.main()
